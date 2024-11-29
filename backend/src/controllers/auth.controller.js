8a31434e15758c31d14b96dc38eaa3c990225dcd
Atik2788 <ashanto653.2@gmail.com>
2024-11-29 06:30:58 +0600

    adding some data in auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,33 @@
-export const singup = (req, res) =>{
-    res.send("Signout route")
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+
+// singup 
+export const singup = async (req, res) =>{
+    const {fullname, email, password} = req.body;
+    try {
+        // hash password
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters"})
+        }
+
+        const user = await User.findOne({email});
+        if(user) return res.status(400).json({message: "Email already exists"});
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullname,
+            email,
+            password : hashPassword
+        })
+        
+    } catch (error) {
+        
+    }
 }
+
+// login 
 export const login = (req, res) =>{
     res.send("Login route")
 }
