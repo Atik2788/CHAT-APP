e9fabf653142a1678dae26b8c2ef2f8bba8eccad
Atik2788 <ashanto653.2@gmail.com>
2024-12-03 21:40:36 +0600

    creating all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./ProfilePage";
+import HomePage from "./HomePage";
+
 const App = () => {
   return (
-    <div >
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup  " element={<SignUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/settings" element={<SettingsPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+      </Routes>
 
     </div>
   );
