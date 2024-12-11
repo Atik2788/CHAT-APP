fcd87b2d86fdffd02d0931002c98338849bb5853
Atik2788 <ashanto653.2@gmail.com>
2024-12-11 20:49:24 +0600

    starting home page design

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,8 +1,13 @@
+import { useChatStore } from "./store/useChatStore"
 
 
 const HomePage = () => {
+  const {selectedUser} = useChatStore
+
   return (
-    <div>HomePage</div>
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">aasdfafwedf</div>
+    </div>
   )
 }
 
