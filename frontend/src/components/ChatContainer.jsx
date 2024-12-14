6a51f918f43438fbac82e61054e082c22ba217ed
Atik2788 <ashanto653.2@gmail.com>
2024-12-14 22:40:43 +0600

    working on chatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore"
 
 const ChatContainer = () => {
 
-  const {messages, getMessages, isUsersLoading } = useChatStore();
+  const {messages, getMessages, isMessagesLoding, selectedUser } = useChatStore();
+
+    
+  useEffect(() =>{
+    getMessages(selectedUser._id)
+  }, [selectedUser._id, getMessages])
+
+  
+  if(isMessagesLoding) return <div>Loading...</div>
 
   return (
     <div>Chat Container</div>
