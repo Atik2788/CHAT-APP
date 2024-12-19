5348e564a66d6097e03ca2834fac9edd02dda729
Atik2788 <ashanto653.2@gmail.com>
2024-12-19 19:59:19 +0600

    updated chatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,10 +3,12 @@ import { useChatStore } from "../store/useChatStore"
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
 
   const {messages, getMessages, isMessagesLoding, selectedUser } = useChatStore();
+  const {authUser} = useAuthStore()
 
     
   useEffect(() =>{
@@ -27,7 +29,14 @@ const ChatContainer = () => {
       <ChatHeader/>
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        
+        {messages.map((message) => (
+          <div
+          key={message._id}
+          className={`chat ${message.sender._id === authUser._id ? "chat-end" : "chat-start" }`}
+          >
+            
+          </div>
+        ))}
       </div>
 
       <MessageInput/>
