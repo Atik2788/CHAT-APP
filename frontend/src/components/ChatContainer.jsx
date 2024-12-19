395cd75fd3e4ecfdcb944ef5a93196f6cda777e7
Atik2788 <ashanto653.2@gmail.com>
2024-12-19 07:25:10 +0600

    starting chat/message section

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -26,7 +26,10 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader/>
 
-      <p>message...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        
+      </div>
+
       <MessageInput/>
 
     </div>
