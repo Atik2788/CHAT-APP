0781c5b556bfdb92159beb17159b2f96e08d0b77
Atik2788 <ashanto653.2@gmail.com>
2024-12-19 23:08:10 +0600

    completed socket.io in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,13 @@ import cors from 'cors'
 import { connectDB  } from './lib/db.js';
 import authRoutes from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
+import { app, server } from './lib/socket.js';
 
 dotenv.config()
-const app = express();
+
+// already created socket.js
+// const app = express();
+
 
 const PORT = process.env.PORT;
 
@@ -24,7 +28,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoute)
 
-app.listen(PORT, () =>{
+server.listen(PORT, () =>{
     console.log('server is running on port: ' + PORT);
     connectDB()
 })
