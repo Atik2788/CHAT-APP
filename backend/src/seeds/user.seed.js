759cd49ba3c0fa877d7cfc1b7292a4b5914e6013
Atik2788 <ashanto653.2@gmail.com>
2024-12-21 05:50:08 +0600

    creating some users by user.seed.js

---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,70 @@
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.model.js";
+
+config();
+
+const seedUsers = [
+  // Female Users
+
+  {
+    email: "isabella.brown@example.com",
+    fullName: "Isabella Brown",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/5.jpg",
+  },
+  {
+    email: "mia.johnson@example.com",
+    fullName: "Mia Johnson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+ 
+
+  // Male Users
+  {
+    email: "james.anderson@example.com",
+    fullName: "James Anderson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+
+  {
+    email: "benjamin.taylor@example.com",
+    fullName: "Benjamin Taylor",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/3.jpg",
+  },
+  {
+    email: "lucas.moore@example.com",
+    fullName: "Lucas Moore",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/4.jpg",
+  },
+  {
+    email: "henry.jackson@example.com",
+    fullName: "Henry Jackson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    email: "daniel.rodriguez@example.com",
+    fullName: "Daniel Rodriguez",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
