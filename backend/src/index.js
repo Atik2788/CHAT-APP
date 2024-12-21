679a457e865b2b22cfeafcf6fb4ff0f5cdc20086
Atik2788 <ashanto653.2@gmail.com>
2024-12-21 22:57:40 +0600

    after completing all parts 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import path from "path";
 
 import { connectDB  } from './lib/db.js';
-import authRoutes from './routes/auth.route.js'
-import messageRoute from './routes/message.route.js'
+import authRoutes from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
 import { app, server } from './lib/socket.js';
 
 dotenv.config()
@@ -15,6 +17,7 @@ dotenv.config()
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
@@ -27,6 +30,15 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoute)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () =>{
     console.log('server is running on port: ' + PORT);
